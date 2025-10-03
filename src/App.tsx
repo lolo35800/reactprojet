27d3239497b249a0c14dd41455bbe3e4dc5fdb80
lolo35800 <francecz35@gmail.com>
2025-10-03 10:48:32 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  const nom = "Alice";
+  return (
+    <div>
+         <h1>Bonjour {nom} 👋</h1>{" "}
+      <p>Bienvenue dans mon premier projet React</p>{" "}
+    </div>
+  );
+}
+export default App;
